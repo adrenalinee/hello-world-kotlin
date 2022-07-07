cde1ce4bbcb7cc71628f601e605f3058066e1e90
malibu <malibu.io@kakaoenterprise.com>
2022-07-07 16:26:48 +0900

    plain.jar 안생기게 수정.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+val jar: Jar by tasks
+jar.enabled = false
+
 repositories {
     mavenCentral()
 }
