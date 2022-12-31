9d04a9a8d8d6db9b5cea5adf5b4990de72318e9e
adrenalinee <adrenalinee31@gmail.com>
2022-12-31 23:03:03 +0900

    error path 추가.

--- src/main/kotlin/com/example/helloworldkotlin/HelloController.kt
+++ src/main/kotlin/com/example/helloworldkotlin/HelloController.kt
@@ -3,6 +3,7 @@ package com.example.helloworldkotlin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import kotlin.RuntimeException
 
 @RestController
 class HelloController {
@@ -10,5 +11,10 @@ class HelloController {
     @GetMapping("/hello")
     fun getHello(): Mono<String> {
         return Mono.just("hello~!")
+    }
+
+    @GetMapping("/error")
+    fun getError(): Mono<String> {
+        return Mono.error { RuntimeException("error!") }
     }
 }
