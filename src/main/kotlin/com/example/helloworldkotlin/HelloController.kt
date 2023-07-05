78ea78c2388b4614c36f2f8d9cb6fb8ad87ebc17
malibu <malibu.io@kakaoenterprise.com>
2023-07-06 00:03:02 +0900

    kustomize 관련 파일 삭제. / 경로에서 서버 이름 보여주도록 수정.

--- src/main/kotlin/com/example/helloworldkotlin/HelloController.kt
+++ src/main/kotlin/com/example/helloworldkotlin/HelloController.kt
@@ -7,6 +7,11 @@ import reactor.core.publisher.Mono
 @RestController
 class HelloController {
 
+    @GetMapping("/")
+    fun index(): Mono<String> {
+        return Mono.just("hello-world-kotlin server")
+    }
+
     @GetMapping("/hello")
     fun getHello(): Mono<String> {
         return Mono.just("hello~!")
