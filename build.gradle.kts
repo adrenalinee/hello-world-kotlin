a9c577c599b2fb1fa2441dbec52b4a6f5dc64eee
malibu <malibu.io@kakaoenterprise.com>
2023-07-07 13:56:24 +0900

    spring-boot, gradle 버전업. root 경로에서 active profiles 를 출력하도록 수정.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,29 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("org.springframework.boot") version "2.7.1"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "3.1.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
 }
 
 group = "com.example"
-//version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+version = "0.0.1-SNAPSHOT"
 
-val jar: Jar by tasks
-jar.enabled = false
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
@@ -32,8 +31,8 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
     }
 }
 
--- src/main/kotlin/com/example/helloworldkotlin/HelloController.kt
+++ src/main/kotlin/com/example/helloworldkotlin/HelloController.kt
@@ -1,15 +1,21 @@
 package com.example.helloworldkotlin
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 @RestController
-class HelloController {
+class HelloController(
+    private val env: Environment
+) {
 
     @GetMapping("/")
     fun index(): Mono<String> {
-        return Mono.just("hello-world-kotlin server")
+        return env.activeProfiles.joinToString(", ")
+            .toMono()
+            .map { "hello-world-kotlin server. active profiles: $it" }
     }
 
     @GetMapping("/hello")
